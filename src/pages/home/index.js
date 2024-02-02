a03c50b30ee1d6aed3e00ac523409ce11e66b33f
GustavoSouza007 <gustavosouzatrindade10@gmail.com>
2024-02-02 16:11:27 -0300

    feat: implement render data on table

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,3 +1,47 @@
+const renderFinancesList = (data) => {
+  const table = document.getElementById("finances-table");
+  data.map((item) => {
+    const tableRow = document.createElement("tr");
+    tableRow.className = "mt smaller";
+
+    const titleTd = document.createElement("td");
+    const titleText = document.createTextNode(item.title);
+    titleTd.appendChild(titleText);
+    tableRow.appendChild(titleTd);
+
+    const categoryTd = document.createElement("td");
+    const categoryText = document.createTextNode(item.name);
+    categoryTd.appendChild(categoryText);
+    tableRow.appendChild(categoryTd);
+
+    const dateTd = document.createElement("td");
+    const dateText = document.createTextNode(
+      new Date(item.date).toDateString("pt-BR", { timezone: "UTC" })
+    );
+    dateTd.appendChild(dateText);
+    tableRow.appendChild(dateTd);
+
+    const valueTd = document.createElement("td");
+    valueTd.className = "center";
+    const valueText = document.createTextNode(
+      new Intl.NumberFormat("pt-BR", {
+        style: "currency",
+        currency: "BRL",
+      }).format(item.value)
+    );
+    valueTd.appendChild(valueText);
+    tableRow.appendChild(valueTd);
+
+    const deleteTd = document.createElement("td");
+    deleteTd.className = "right";
+    const deleteText = document.createTextNode("Deletar");
+    deleteTd.appendChild(deleteText);
+    tableRow.appendChild(deleteTd);
+
+    table.appendChild(tableRow);
+  });
+};
+
 const renderFinanceElements = (data) => {
   const totalItems = data.length;
   const revenues = data
@@ -67,6 +111,8 @@ const onLoadFinancesData = async () => {
       }
     );
     const data = await result.json();
+    renderFinanceElements(data);
+    renderFinancesList(data);
     return data;
   } catch (error) {
     return { error };
